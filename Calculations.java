a1c1fe8cb56f9edfd571162c58b4a468b2fa2ee4
Rashed1Alm <b00076049@aus.edu>
2020-06-11 23:09:28 +0400

    Subtraction implemented

--- Calculations.java
+++ Calculations.java
@@ -9,7 +9,9 @@ public class Calculations {
 		System.out.println("The sum is= "+ sum);
 		
 		//member_B add the feature of subtracting x from y and print the result
-		
+		int sub = x-y;
+		System.out.println("The subtraction is= "+ sub);
+
 		//member_C add the feature of dividing x by y and print the result
 		double div = x/y;
 		system.out.println("The divison average is"+ div);
